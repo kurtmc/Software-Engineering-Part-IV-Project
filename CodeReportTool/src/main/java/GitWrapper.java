3c42285205000b0ab56b6c81bd763c0e01044820
Kurt McAlpine <kurt@linux.com>
2015-08-04 12:06:25 +1200

    Creates a simple report about the repo

---
+++ CodeReportTool/src/main/java/GitWrapper.java
@@ -0,0 +1,91 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.text.DateFormat;
+import java.util.Locale;
+
+
+public class GitWrapper {
+
+	public static String execShell(String command) {
+		try {
+			Process p = Runtime.getRuntime().exec(new String[]{"bash","-c",command});
+			BufferedReader reader = 
+				  new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+			StringBuilder sb = new StringBuilder();
+			String line = "";           
+			while ((line = reader.readLine())!= null) {
+				    sb.append(line + "\n");
+			}
+			return sb.toString();
+		} catch (Exception e) {
+			return null;
+		}
+
+	}
+
+	public static String[] getHashes(String path) {
+		String result = execShell("cd " + path + "; git log --pretty=oneline --abbrev-commit");
+		return result.split("\n");
+	}
+
+	public static Date getDate(String path, String hash) {
+		try {
+			String dateString = execShell("cd " + path + "; git show " + hash + " --pretty=format:\"%ad\" --date=iso").split("\n")[0];
+
+			DateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss Z", Locale.ENGLISH);
+			Date date = format.parse(dateString);
+
+			return date;
+		} catch (Exception e) {
+		}
+
+		return null;
+	}
+
+	public static double charsPerMinute(String path) {
+		String[] hashes = getHashes(path);
+
+		String lastHash = hashes[0];
+		String firstHash = hashes[hashes.length - 1];
+
+		Date last = getDate(path, lastHash);
+		Date first = getDate(path, firstHash);
+
+		int lastChars = getJava(path, lastHash).length();
+		int firstChars = getJava(path, firstHash).length();
+
+		int charDelta = lastChars = firstChars;
+
+		long timeDelta = (last.getTime()-first.getTime())/1000;
+
+		double charPerSecond = (double) charDelta / (double) timeDelta;
+
+		return charPerSecond * 60;
+	}
+
+	public static String getJava(String path, String hash) {
+		execShell("cd " + path + "; git checkout " + hash);
+
+		String java = execShell("find . -name '*.java' -exec cat {} \\;");
+
+		execShell("cd " + path + "; git checkout master");
+
+		return java;
+	}
+
+	public static Date startTime(String path) {
+		String[] hashes = getHashes(path);
+		String startHash = hashes[hashes.length - 1];
+
+		return getDate(path, startHash);
+	}
+	public static Date endTime(String path) {
+		String[] hashes = getHashes(path);
+		String startHash = hashes[0];
+
+		return getDate(path, startHash);
+	}
+}
--- CodeReportTool/src/main/java/Main.java
+++ CodeReportTool/src/main/java/Main.java
@@ -1,5 +1,33 @@
+import java.util.Date;
+
 public class Main {
+
+    private static void print(Object o) {
+        System.out.println(o);
+    }
+    /**
+     * Handles the command line interface to generating reports based on git repos
+     * @param args
+     */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        if (args.length < 1) {
+            print("Not enough arguments. You need to specify the git repo");
+	    System.exit(1);
+        }
+
+	String repo = args[0];
+
+
+	Date startTime = GitWrapper.startTime(repo);
+	System.out.println("Test started at " + startTime);
+
+	Date endTime = GitWrapper.endTime(repo);
+	System.out.println("Test ended at " + endTime);
+
+	System.out.println("The test lasted " + (endTime.getTime() - startTime.getTime())/ (1000 * 60) + " minutes");
+
+	double chars = GitWrapper.charsPerMinute(repo);
+	System.out.println("Characters per minute " + chars);
     }
 }
